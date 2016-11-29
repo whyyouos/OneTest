c92ebb1faadc7b193500ea0ba3f4ed3296cb465e
la <la@eeepay.cn>
2016-11-29 17:28:37 +0800

    【add】合并分支

--- app/src/main/java/com/test/testproject/MainActivity.java
+++ app/src/main/java/com/test/testproject/MainActivity.java
@@ -1,25 +1,20 @@
 package com.test.testproject;
 
 import android.content.Context;
-import android.provider.Settings;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import static android.view.View.OnClickListener;
 
 /**
- * 类说明:master分支
+ * 类说明:master和branch_one分支合并
  *@data 2016年11月29日 15:14:55
  *@name LiangAn
- * branch_one
+ *
  */
 public class MainActivity extends ActionBarActivity implements OnClickListener{
     private String TAG = "MainActivity.class";
